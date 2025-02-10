a5f85ab9a8a5dae648608e9b0d6af166265535de
Pavithra214 <pavithra.it214@gmail.com>
2025-02-10 14:15:09 +0530

    PropertiesModel
    Properties required for the project

---
+++ JSBilling.Model/Admin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSBilling.Model
+{
+    public class Admin
+    {
+        public int id {  get; set; }
+        public string fullname { get; set; }
+
+        public long Phoneno { get; set; }
+
+        public DateOnly CheckIn { get; set; }
+
+        public int Notes { get; set; }
+
+        public int Demo { get; set; }
+
+        public int Project { get; set; }
+
+
+    }
+}
---
+++ JSBilling.Model/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace JSBilling.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string email { get; set; }
+        public long phone { get; set; }
+        public string password { get; set; }
+
+        public int role { get; set; }
+
+    }
+}
